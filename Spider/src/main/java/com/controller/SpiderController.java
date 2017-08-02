89a6990fb55aaaa2393474c5e1f100695c691958
MiroGene <inkysun@163.com>
2017-08-02 11:08:21 +0800

    测试提交及合并
    修改了spiderController和spiderSetting的类职能

--- Spider/src/main/java/com/controller/SpiderController.java
+++ Spider/src/main/java/com/controller/SpiderController.java
@@ -12,33 +12,17 @@ import sun.rmi.runtime.Log;
 /**
  * Created by Administrator on 2017/7/17.
  */
-public class SpiderController extends BreadthCrawler {
+public class SpiderController {
     /**
      * allocate work
      */
     private static final Logger logger = LoggerFactory.getLogger(SpiderController.class);
 
 
-    public SpiderController(String crawlPath, boolean autoParse) {
-        super(crawlPath, autoParse);
-    }
 
 
     public void initializeSpider(){
-        //html源码获取(文件下载)
-        //文件操作
-        //html源码解析(数据清洗)
-        //线程池
-        //URL生成器(遍历器)
-        //消息机制(各组件通信)
-        /*start page*/
-        this.addSeed("http://news.hfut.edu.cn/list-1-1.html");
-        /*fetch url like http://news.hfut.edu.cn/show-xxxxxxhtml*/
-        this.addRegex("http://news.hfut.edu.cn/show-.*html");
-        /*do not fetch jpg|png|gif*/
-        this.addRegex("-.*\\.(jpg|png|gif).*");
-        /*do not fetch url contains #*/
-        this.addRegex("-.*#.*");
+
 
     }
 
@@ -63,10 +47,6 @@ public class SpiderController extends BreadthCrawler {
         //save parsed data
 
 
-
-    }
-    @Override
-    public void visit(Page page, CrawlDatums crawlDatums) {
 
     }
 }
--- Spider/src/main/java/com/controller/SpiderSetting.java
+++ Spider/src/main/java/com/controller/SpiderSetting.java
@@ -1,5 +1,8 @@
 package com.controller;
 
+import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
+import cn.edu.hfut.dmic.webcollector.model.Page;
+import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import com.Spider;
 import com.vo.SpiderConfig;
 import org.slf4j.Logger;
@@ -8,12 +11,34 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by Gene on 2017/8/1.
  */
-public class SpiderSetting {
+public class SpiderSetting extends BreadthCrawler {
     private static final Logger logger = LoggerFactory.getLogger(SpiderSetting.class);
-    public SpiderSetting(){
+
+    public SpiderSetting(String crawlPath, boolean autoParse) {
+        super(crawlPath, autoParse);
+        //若不同，替换配置文件
         //读取配置文件
     }
-    public SpiderSetting(SpiderConfig spiderConfig){
-        //若不同，替换配置文件
+
+    public void initializeSpider(){
+        //html源码获取(文件下载)
+        //文件操作
+        //html源码解析(数据清洗)
+        //线程池
+        //URL生成器(遍历器)
+        //消息机制(各组件通信)
+        /*start page*/
+        this.addSeed("http://news.hfut.edu.cn/list-1-1.html");
+        /*fetch url like http://news.hfut.edu.cn/show-xxxxxxhtml*/
+        this.addRegex("http://news.hfut.edu.cn/show-.*html");
+        /*do not fetch jpg|png|gif*/
+        this.addRegex("-.*\\.(jpg|png|gif).*");
+        /*do not fetch url contains #*/
+        this.addRegex("-.*#.*");
+    }
+
+    @Override
+    public void visit(Page page, CrawlDatums crawlDatums) {
+
     }
 }
