d19234813e52d7d99d20877a4dd8fd16468f284e
MiroGene <inkysun@163.com>
2017-08-03 11:04:43 +0800

    modified:   Spider/src/main/java/com/spider/controller/SpiderController.java

--- Spider/src/main/java/com/Spider.java
+++ Spider/src/main/java/com/Spider.java
@@ -1,31 +1,63 @@
 package com;
 
+import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
+import cn.edu.hfut.dmic.webcollector.model.Page;
+import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import com.spider.vo.ConditionVo;
 
 /**
  * Created by Gene on 2017/7/28.
  * 控制整体程序的运行
  */
-public class Spider {
+public class Spider extends BreadthCrawler {
 
 
     private ConditionVo conditionVoObject;
     private String  conditionVoJson;
-    //传递对象
-    public Spider(ConditionVo conditionVo){
-        this.conditionVoObject = conditionVo;
-    }
-    //模块间以json传递参数（需解析）
-    public Spider(String conditionVoJson){
-        this.conditionVoJson = conditionVoJson;
-    }
-    //使用默认配置（需要读取配置文件）
-    public Spider(){
 
+    public Spider(String crawlPath, boolean autoParse) {
+        super(crawlPath, autoParse);
     }
+
+    /*    //传递对象
+        public Spider(ConditionVo conditionVo){
+            this.conditionVoObject = conditionVo;
+        }
+        //模块间以json传递参数（需解析）
+        public Spider(String conditionVoJson){
+            this.conditionVoJson = conditionVoJson;
+        }
+        //使用默认配置（需要读取配置文件）
+        public Spider(){
+
+        }*/
     public static void main(String[] args) {
         //获取消费者
 
     }
 
+    /**
+     * 设置代理
+     */
+    private static void setProxy(){}
+
+    /**
+     * 自定义http请求
+     */
+    private static void setHttpRequest(){}
+
+    /**
+     * 爬取页面类型(html?JS?photo?)
+     */
+    private static void setPageKind(){}
+
+    /**
+     * 设置文件存放地址(图片，原始数据等)
+     */
+    private static void savePath(String path){}
+
+    @Override
+    public void visit(Page page, CrawlDatums crawlDatums) {
+
+    }
 }
--- Spider/src/main/java/com/spider/controller/SpiderController.java
+++ Spider/src/main/java/com/spider/controller/SpiderController.java
@@ -1,72 +1,6 @@
-<<<<<<< HEAD:Spider/src/main/java/com/controller/SpiderController.java
-package com.controller;
-
-<<<<<<< HEAD
-import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
-import cn.edu.hfut.dmic.webcollector.model.Page;
-import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
-import com.service.OriginalDataService;
-import com.service.impl.OriginalDataServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sun.rmi.runtime.Log;
-
-=======
->>>>>>> first module
-/**
- * Created by Administrator on 2017/7/17.
- */
-public class SpiderController {
-    /**
-     * allocate work
-     */
-<<<<<<< HEAD
-    private static final Logger logger = LoggerFactory.getLogger(SpiderController.class);
-
-
-
-
-    public void initializeSpider(){
-
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-    /**
-     * 分配工作
-     */
-    public void allocateWork(){
-        //新建一个webcollector
-
-        //allocate thread
-        //control the visit url
-        //begin fetcher(if fetcher fail, control the visit url)
-        //saveOriginalData
-        OriginalDataService  ods = new OriginalDataServiceImpl();
-
-
-        //begin parse
-        //save parsed data
-
-
-
-=======
-    public void allocateWork(){
-        //allocate thread
-        //control the visit url
-        //begin fetcher(if fetcher fail, control the visit url)
-
-        //begin parse
-        //
->>>>>>> first module
-    }
-}
-=======
 package com.spider.controller;
 
+import com.Spider;
 import com.spider.service.OriginalDataService;
 import com.spider.service.impl.OriginalDataServiceImpl;
 import org.slf4j.Logger;
@@ -81,18 +15,27 @@ public class SpiderController {
      */
     private static final Logger logger = LoggerFactory.getLogger(SpiderController.class);
 
-
+    private Spider spider;
 
 
     public void initializeSpider(){
-
+        this.spider = new Spider("",true);
 
     }
+
 
     public static void main(String[] args) {
-
+        SpiderController spiderController = new SpiderController();
+        spiderController.start();
     }
 
+    public void start(){
+        try {
+            spider.start(5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * 分配工作
      */
@@ -113,4 +56,3 @@ public class SpiderController {
 
     }
 }
->>>>>>> fd265b8c42dbf2e8cb1b11513239065ebd7e7628:Spider/src/main/java/com/spider/controller/SpiderController.java
