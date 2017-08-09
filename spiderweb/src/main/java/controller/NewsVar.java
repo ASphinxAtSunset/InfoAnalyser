0aa6f9f6d6e44f8ff99c2a3b2c9d3b784618ae7b
NY_XU <hrbuswxuli@163.com>
2017-08-09 17:03:11 +0800

    index.jsp和NewsVar.java添加测试

--- spiderweb/src/main/java/controller/NewsVar.java
+++ spiderweb/src/main/java/controller/NewsVar.java
@@ -1,16 +1,31 @@
 package controller;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.spider.service.OriginalDataService;
 import com.spider.vo.ConditionVo;
+import com.uniontrip.base.web.ResponseBean;
+import com.uniontrip.lvzhi.statistics.StatisticsController;
 
 /**
  * Created by Gene on 2017/7/25.
  */
+@Controller
+@RequestMapping(value = "/user")
 public class NewsVar {
-
-    public String setSpiderVar(){
-        ConditionVo conditionVo = new ConditionVo();
-        //在此处转换为json
-        return "";
+	private static Logger log = LoggerFactory.getLogger(StatisticsController.class);
+	
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+    public ResponseBean generateqrcode(@RequestBody Map<String, Object> param){
     }
 }
--- spiderweb/web/index.jsp
+++ spiderweb/web/index.jsp
@@ -1,20 +1,17 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Administrator
-  Date: 2017/7/24
-<<<<<<< HEAD
-  Time: 17:18
-=======
-  Time: 11:10
->>>>>>> first module
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Spring MVC Test</title>
   </head>
   <body>
-  $END$
+  <div>
+  	<form action="/user/login" method="post">
+  		用户名：<input type="text" name="username" />
+  		<br/>
+  		密码：<input type="password" name="password" />
+  		<br/>
+  		<input type="submit" value="登录" />
+  	</form>
+  </div>
   </body>
 </html>
