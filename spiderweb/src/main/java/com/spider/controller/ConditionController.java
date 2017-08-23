1422ff289f1820cd9fc0af38218df2763fb58bc9
NY_XU <hrbuswxuli@163.com>
2017-08-23 17:09:17 +0800

    start condition page

---
+++ spiderweb/src/main/java/com/spider/controller/ConditionController.java
@@ -0,0 +1,14 @@
+package com.spider.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/security/qrcode")
+public class ConditionController {
+	public void getAllConditions(Map<String,Object> map){
+		
+	}
+}
